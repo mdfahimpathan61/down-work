186198c7ac149520c58d2e939adacc390d8bbd7b
MD Fahim Pathan <mdfahimpathan61@gmail.com>
2026-08-10 00:54:10 +0600

    update navbar, show user picture and name, modify dorpdown menu

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,24 +1,23 @@
 import React, { useContext } from "react";
-import { NavLink, useNavigate } from "react-router";
+import { Link, NavLink, useNavigate } from "react-router";
 import logo from "/logo (1).png";
 import "./navbar.css";
-import { CiMenuFries } from "react-icons/ci";
+import { CiLogin, CiLogout, CiMenuFries } from "react-icons/ci";
 import { AuthContext } from "../../Provider/AuthProvider";
 
 const Navbar = () => {
-  const {activeUser, signout} = useContext(AuthContext)
-  const navigate = useNavigate()
+  const { activeUser, signout } = useContext(AuthContext);
+  console.log(activeUser);
+  const navigate = useNavigate();
 
+  const handleSignOut = () => {
+    if (activeUser) {
+      signout();
+    } else {
+      navigate("/auth/login");
+    }
+  };
 
-  const handleSignOut =() =>{
-    if(activeUser){
-      signout()
-    }
-    else{
-      navigate("/auth/login")
-    }
-  } 
-  
   const list = (
     <>
       <NavLink to={"/"}>
@@ -38,38 +37,89 @@ const Navbar = () => {
     <div className="max-w-360 mx-auto shadow-lg shadow-blue-50 relative z-50">
       <div className="navbar bg-base-100 ">
         <div className="navbar-start">
-            <img className="md:w-30 w-20" src={logo} alt="" />
+          <img className="md:w-30 w-20" src={logo} alt="" />
         </div>
 
-        <div className="navbar-center hidden lg:flex">
+        <div className="navbar-center hidden md:flex">
           <ul className="menu menu-horizontal px-1 gap-7 text-lg">{list}</ul>
         </div>
-        
-          <div className="navbar-end">
-            <div>
-              {
-                activeUser?
-                <div><img src={activeUser?.imageUrl} alt="" /> </div>:""
+
+        <div className="navbar-end">
+          {
+            !activeUser && <div className="">
+            <Link to={"/auth/login"} className="hidden  sm:block ">
+                  <p className="text-accent hover:text-primary flex items-center gap-1 "><CiLogin /> Log in/Registration </p>
+                </Link>
+          </div>
+          }
+
+          <div className="dropdown dropdown-end hidden  sm:block">
+            <div tabIndex={0} className=" m-1">
+              {activeUser && (
+                <div>
+                  <img
+                    className="w-8 h-8 md:w-11 md:h-11 rounded-full  border-3  border-gray-100"
+                    src={activeUser?.photoURL}
+                    alt=""
+                  />
+                </div>
+              ) 
               }
-              <button className="btn" onClick={handleSignOut}>{activeUser ?"Sign out":"Log in"}</button>
             </div>
-            <div className="dropdown dropdown-end">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost lg:hidden"
+            <div
+              tabIndex="-1"
+              className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm py-4 "
+            >
+              <img
+                className="w-8 h-8 md:w-11 md:h-11 rounded-full  border-3 mx-auto border-gray-100"
+                src={activeUser?.photoURL}
+                alt=""
+              />
+              <h4 className="font-bold text-center mb-2">
+                {activeUser?.displayName}
+              </h4>
+              <hr className=" border-gray-200 my-2" />
+              <button
+                className="mx-auto  mt-2 gap-1 text-secondary hover:text-primary flex items-center"
+                onClick={handleSignOut}
               >
-                <CiMenuFries />
-              </div>
-              <ul
-                tabIndex="5"
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 max-w-52 p-2 shadow shadow-blue-200"
-              >
-                {list}
-              </ul>
+                <CiLogout /> Log out
+              </button>
             </div>
           </div>
-        
+
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
+              <CiMenuFries />
+            </div>
+            <div
+              tabIndex="5"
+              className="dropdown-content menu bg-base-100 rounded-box z-1 w-32 p-2 shadow-sm py-4"
+            >
+              {activeUser && (
+                <div>
+                  <img
+                    className="w-8 h-8 md:w-11 md:h-11 rounded-full  border-3 mx-auto border-gray-100"
+                    src={activeUser?.photoURL}
+                    alt=""
+                  />
+                  <h4 className="font-bold text-sm text-center mb-2">
+                    {activeUser?.displayName}
+                  </h4>
+                  <hr className=" border-gray-200" />
+                </div>
+              )}
+              {list}
+              <hr className=" border-gray-200 my-2" />
+              <button
+                className="   gap-1 text-secondary hover:text-primary flex items-center"
+                onClick={handleSignOut}
+              >
+                <CiLogin /> {activeUser ?"Log out":"Log in"}
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
